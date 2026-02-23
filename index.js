ed1b175e805c271dba7b6b5f48a207be17b35159
Stijn Luiken <stijnphilipluiken@gmail.com>
2026-02-23 17:42:03 +0100

    Created basic canvas

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const canvas = document.getElementById("drawing-canvas")
+const context = canvas.getContext("2d")
+
+const canvas_height = 500
+const canvas_width = 800
+
+const canvas_background_color = "#f0f0f0"
+
+
+
+
+canvas.height = canvas_height
+canvas.width = canvas_width
+
+canvas.style.background = canvas_background_color
+
+
